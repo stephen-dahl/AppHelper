5cb0bfc7676cd18c2d64a75cd3c395d117351df6
Jeremy Zongker <jeremy@zongker.net>
2023-08-31 23:38:22 -0500

    Updated readme with link instructions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Helper functions shared by React and NextJS Apps
 
 ## To Test
 
-1. In the AppHelper folder run `npm link ../../ChurchApps/ChurchAppsWeb/node_modules/react ../../ChurchApps/ChurchAppsWeb/node_modules/react-dom` plugging, in the path to the app you plan to test with.
+1. In the AppHelper folder run `npm link ../../Chums/ChumsApp/node_modules/react ../../Chums/ChumsApp/node_modules/react-dom ../../Chums/ChumsApp/node_modules/react-router-dom` plugging, in the path to the app you plan to test with.
 2. Run `npm build` and `npm link` in AppHelper folder
 3. Switch to the test project folder and run `npm link @churchapps/apphelper` to point to your local copy
 4. Run `npm start` or `npm run dev` within your test project
--- src/components/material/NavItem.tsx
+++ src/components/material/NavItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Icon, ListItemButton, ListItemIcon, ListItemText, styled, Tooltip } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 interface Props {
   url?: string;
@@ -20,13 +20,11 @@ const StyledNavLink = styled(NavLink)({
 });
 
 export const NavItem: React.FC<Props> = (props) => {
-  /*
   let isReact = false;
   try {
     const a = typeof useLocation();
     if (a !== null) isReact = true;
   } catch { }
-*/
 
   const getIcon = () => <Icon>{props.icon}</Icon>
 
@@ -38,7 +36,6 @@ export const NavItem: React.FC<Props> = (props) => {
   </ListItemButton>)
 
   if (props.router) return (<a href={props.url} target={props.target} onClick={(e) => { e.preventDefault(); props.onClick ? props.onClick() : props.router.push(props.url) }} className={(props.selected) ? "selected" : ""}>{getLinkContents()}</a>)
-  //else if (props.external || !isReact) return (<a href={props.url} target={props.target} rel="noreferrer" style={{ textDecoration: "none" }} className={(props.selected) ? "selected" : ""} onClick={(e) => { e.preventDefault(); props.onClick ? props.onClick() : window.location.href = props.url }}>{getLinkContents()}</a>)
-  else if (props.external) return (<a href={props.url} target={props.target} rel="noreferrer" style={{ textDecoration: "none" }} className={(props.selected) ? "selected" : ""} onClick={(e) => { e.preventDefault(); props.onClick ? props.onClick() : window.location.href = props.url }}>{getLinkContents()}</a>)
+  else if (props.external || !isReact) return (<a href={props.url} target={props.target} rel="noreferrer" style={{ textDecoration: "none" }} className={(props.selected) ? "selected" : ""} onClick={(e) => { e.preventDefault(); props.onClick ? props.onClick() : window.location.href = props.url }}>{getLinkContents()}</a>)
   else return (<StyledNavLink to={props.url || "about:blank"} target={props.target} className={(props.selected) ? "selected" : ""} onClick={props.onClick ? (e) => { e.preventDefault(); props.onClick() } : null}>{getLinkContents()}</StyledNavLink>)
 };
