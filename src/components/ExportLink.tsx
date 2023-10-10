8e8753f7e6cbdb60338b002087289366e1bd5f88
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2023-10-10 16:35:36 +0530

    export dates in people.csv (#3)
    * export dates in people.csv
    
    * improve

--- src/components/ExportLink.tsx
+++ src/components/ExportLink.tsx
@@ -11,9 +11,11 @@ interface Props {
 
 export const ExportLink: React.FC<Props> = (props) => {
 
+  const people = [...props.data];
+
   const getHeaders = () => {
     let result = [];
-    if (props.data?.length > 0) {
+    if (people?.length > 0) {
       let names = getAllPropertyNames();
       for (let i = 0; i < names.length; i++) { result.push({ label: names[i], key: names[i] }); }
     }
@@ -22,8 +24,11 @@ export const ExportLink: React.FC<Props> = (props) => {
 
   const getAllPropertyNames = () => {
     let result = [];
-    for (let i = 0; i < props.data.length; i++) {
-      let propertyNames = getPropertyNames("", props.data[i]);
+    for (let i = 0; i < people.length; i++) {
+      let p = {...people[i]}
+      p.birthDate = p.birthDate ? new Date(p.birthDate).toISOString() : null;
+      p.anniversary = p.anniversary ? new Date(p.anniversary).toISOString() : null;
+      let propertyNames = getPropertyNames("", p);
       for (let j = 0; j < propertyNames.length; j++) if (result.indexOf(propertyNames[j]) === -1) result.push(propertyNames[j]);
     }
     return result.sort();
@@ -50,11 +55,11 @@ export const ExportLink: React.FC<Props> = (props) => {
     return result;
   }
 
-  if (!props.data || props.data?.length === 0) return null;
+  if (!people || people?.length === 0) return null;
   else {
     let items = [];
     if (props.spaceBefore) items.push(" ");
-    items.push(<CSVLink key={props.filename} data={props.data} headers={getHeaders()} filename={props.filename || "export.csv"}> <Button><Icon>file_download</Icon></Button></CSVLink>);
+    items.push(<CSVLink key={props.filename} data={people} headers={getHeaders()} filename={props.filename || "export.csv"}> <Button><Icon>file_download</Icon></Button></CSVLink>);
     if (props.spaceAfter) items.push(" ");
     return (<>{items}</>);
   }
