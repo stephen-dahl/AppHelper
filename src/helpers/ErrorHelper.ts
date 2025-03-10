6678c113e82d86c0a30f68339e0cb94d20980651
Jeremy Zongker <jeremy@zongker.net>
2025-03-09 22:31:27 -0500

    Updated /clientErrors path

--- src/helpers/ErrorHelper.ts
+++ src/helpers/ErrorHelper.ts
@@ -4,35 +4,34 @@ import { ApiHelper } from "./ApiHelper";
 
 export class ErrorHelper {
 
-  static getAppData:() => { churchId: string, userId: string, originUrl: string, application: string};
-  static customErrorHandler:(errorLog:ErrorLogInterface) => void;
+	static getAppData: () => { churchId: string, userId: string, originUrl: string, application: string };
+	static customErrorHandler: (errorLog: ErrorLogInterface) => void;
 
-  static init = (getAppData:() => ErrrorAppDataInterface, customErrorHandler:(errorLog:ErrorLogInterface) => void) => {
-    ErrorHelper.getAppData = getAppData;
-    ErrorHelper.customErrorHandler = customErrorHandler;
-  }
+	static init = (getAppData: () => ErrrorAppDataInterface, customErrorHandler: (errorLog: ErrorLogInterface) => void) => {
+		ErrorHelper.getAppData = getAppData;
+		ErrorHelper.customErrorHandler = customErrorHandler;
+	}
 
-  static logError = (errorType:string, message:string, details:string) => {
-    if (this.getAppData)
-    {
-      const data = this.getAppData();
-      const log:ErrorLogInterface = {
-        application: data.application,
-        errorTime: new Date(),
-        userId: data.userId,
-        churchId: data.churchId,
-        originUrl: data.originUrl,
-        errorType: errorType,
-        message: message,
-        details: details
-      }
+	static logError = (errorType: string, message: string, details: string) => {
+		if (this.getAppData) {
+			const data = this.getAppData();
+			const log: ErrorLogInterface = {
+				application: data.application,
+				errorTime: new Date(),
+				userId: data.userId,
+				churchId: data.churchId,
+				originUrl: data.originUrl,
+				errorType: errorType,
+				message: message,
+				details: details
+			}
 
-      console.log("ERROR LOG", log);
+			console.log("ERROR LOG", log);
 
-      if (log.errorType==="401" && log.message.indexOf("/users/login")>-1) return;
-      ApiHelper.postAnonymous("/errors", [log], "MembershipApi");
-      if (ErrorHelper.customErrorHandler) ErrorHelper.customErrorHandler(log);
-    }
-  }
+			if (log.errorType === "401" && log.message.indexOf("/users/login") > -1) return;
+			ApiHelper.postAnonymous("/clientErrors", [log], "MembershipApi");
+			if (ErrorHelper.customErrorHandler) ErrorHelper.customErrorHandler(log);
+		}
+	}
 
 }
