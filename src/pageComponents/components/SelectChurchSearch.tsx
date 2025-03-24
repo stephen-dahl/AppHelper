2fbcc22f71f52764ac6ed7c85063547933a31d0d
Jeremy Zongker <jeremy@zongker.net>
2025-03-24 08:45:06 -0500

    Removed unnecessary esapce() from church search post

--- src/pageComponents/components/SelectChurchSearch.tsx
+++ src/pageComponents/components/SelectChurchSearch.tsx
@@ -8,64 +8,64 @@ import { SelectableChurch } from "./SelectableChurch";
 import { SelectChurchRegister } from "./SelectChurchRegister";
 
 interface Props {
-  selectChurch: (churchId: string) => void,
-  registeredChurchCallback?: (church: ChurchInterface) => void,
-  appName: string
+	selectChurch: (churchId: string) => void,
+	registeredChurchCallback?: (church: ChurchInterface) => void,
+	appName: string
 }
 
 export const SelectChurchSearch: React.FC<Props> = (props) => {
-  const [searchText, setSearchText] = React.useState("");
-  const [churches, setChurches] = React.useState<ChurchInterface[]>(null);
-  const [showRegister, setShowRegister] = React.useState(false);
+	const [searchText, setSearchText] = React.useState("");
+	const [churches, setChurches] = React.useState<ChurchInterface[]>(null);
+	const [showRegister, setShowRegister] = React.useState(false);
 
-  const handleSubmit = (e: React.MouseEvent) => {
-    if (e !== null) e.preventDefault();
-    let term = escape(searchText.trim());
-    ApiHelper.post("/churches/search", { name: term }, "MembershipApi").then(data => setChurches(data));
-  }
+	const handleSubmit = (e: React.MouseEvent) => {
+		if (e !== null) e.preventDefault();
+		let term = searchText.trim();
+		ApiHelper.post("/churches/search", { name: term }, "MembershipApi").then(data => setChurches(data));
+	}
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.currentTarget.value);
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.currentTarget.value);
 
-  const handleKeyDown = (e: React.KeyboardEvent<any>) => { if (e.key === "Enter") { e.preventDefault(); handleSubmit(null); } }
+	const handleKeyDown = (e: React.KeyboardEvent<any>) => { if (e.key === "Enter") { e.preventDefault(); handleSubmit(null); } }
 
-  const handleRegisterClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (window.confirm(Locale.label("selectChurch.confirmRegister"))) {
-      setShowRegister(true);
-    }
-  }
+	const handleRegisterClick = (e: React.MouseEvent) => {
+		e.preventDefault();
+		if (window.confirm(Locale.label("selectChurch.confirmRegister"))) {
+			setShowRegister(true);
+		}
+	}
 
-  const getRegisterLink = () => (
-    <div>
-      <a style={{ display: "block", textAlign: "center" }} href="about:blank" onClick={handleRegisterClick}>
-        {Locale.label("selectChurch.register")}
-      </a>
-    </div>
-  )
+	const getRegisterLink = () => (
+		<div>
+			<a style={{ display: "block", textAlign: "center" }} href="about:blank" onClick={handleRegisterClick}>
+				{Locale.label("selectChurch.register")}
+			</a>
+		</div>
+	)
 
-  const getChurches = () => {
-    const result: JSX.Element[] = [];
-    churches.forEach(church => {
-      result.push(<SelectableChurch church={church} selectChurch={props.selectChurch} />);
-    });
-    result.push(getRegisterLink());
-    return result;
-  }
+	const getChurches = () => {
+		const result: JSX.Element[] = [];
+		churches.forEach(church => {
+			result.push(<SelectableChurch church={church} selectChurch={props.selectChurch} />);
+		});
+		result.push(getRegisterLink());
+		return result;
+	}
 
-  const getResults = () => {
-    if (churches === null) return;
-    else if (churches.length === 0) return <><p>{Locale.label("selectChurch.noMatches")}</p>{getRegisterLink()}</>
-    else return getChurches();
-  }
+	const getResults = () => {
+		if (churches === null) return;
+		else if (churches.length === 0) return <><p>{Locale.label("selectChurch.noMatches")}</p>{getRegisterLink()}</>
+		else return getChurches();
+	}
 
-  if (showRegister) return (<SelectChurchRegister selectChurch={props.selectChurch} registeredChurchCallback={props.registeredChurchCallback} appName={props.appName} initialChurchName={searchText} />)
-  else return (
-    <>
-      <TextField fullWidth name="searchText" label="Name" value={searchText} onChange={handleChange} onKeyDown={handleKeyDown}
-        InputProps={{ endAdornment: <Button variant="contained" id="searchButton" data-cy="search-button" onClick={handleSubmit}>{Locale.label("common.search")}</Button> }}
-      />
-      {getResults()}
-    </>
+	if (showRegister) return (<SelectChurchRegister selectChurch={props.selectChurch} registeredChurchCallback={props.registeredChurchCallback} appName={props.appName} initialChurchName={searchText} />)
+	else return (
+		<>
+			<TextField fullWidth name="searchText" label="Name" value={searchText} onChange={handleChange} onKeyDown={handleKeyDown}
+				InputProps={{ endAdornment: <Button variant="contained" id="searchButton" data-cy="search-button" onClick={handleSubmit}>{Locale.label("common.search")}</Button> }}
+			/>
+			{getResults()}
+		</>
 
-  );
+	);
 };
